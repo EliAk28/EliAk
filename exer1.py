aeb2dce002f74e2b5fb68ffafe2f21c07e9de73d
Elijah <haykayelijah@gmail.com>
2025-07-22 11:34:52 +0100

    exer1.py

--- exer1.py
+++ exer1.py
@@ -8,3 +8,13 @@ b = int(input("Enter second number: "))
 
 print(f"The sum of {a} and {b} is {a + b}")
 print("first modification for 2nd commit")
+# Simple Python script
+
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(f"The sum of {a} and {b} is {a + b}")
+print("first modification for 2nd commit")
