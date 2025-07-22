e0ff9adef27199c2bbf892bf14bebd798e22e24a
Elijah <haykayelijah@gmail.com>
2025-07-22 12:59:22 +0100

    Merged origin/master and added exer4.py

---
+++ exer4.py
@@ -0,0 +1,7 @@
+# Reverse a word entered by the user
+
+word = input("Enter a word: ")
+reversed_word = word[::-1]
+print(f"Reversed word: {reversed_word}")
+#...........................................
+print("first modification for 2nd commit")
